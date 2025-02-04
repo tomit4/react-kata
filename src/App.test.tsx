0f26ccfa05f9cddeba675057adb39c40b1f04d03
tomit4 <mosssap@gmail.com>
2025-02-03 19:46:52 -0800

    :test_tube: Set up generic tests

---
+++ src/App.test.tsx
@@ -0,0 +1,13 @@
+/**
+  * @jest-environment jsdom
+  */
+
+import { render } from "@testing-library/react";
+import { describe, it } from "vitest";
+import App from "./App";
+
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    render(<App />);
+  });
+});
--- src/components/BookingForm/BookingForm.test.tsx
+++ src/components/BookingForm/BookingForm.test.tsx
@@ -1,1 +1,7 @@
-export {};
+import { describe, it, expect } from "vitest";
+
+describe("Boilerplate Test", () => {
+  it("should pass", () => {
+    expect(true).toBe(true);
+  });
+});
