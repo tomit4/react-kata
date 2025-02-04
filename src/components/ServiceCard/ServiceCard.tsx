5ec72be7498a5818e1d0e0a80fb288f59b8a66da
tomit4 <mosssap@gmail.com>
2025-02-03 17:03:05 -0800

    :bento: Added brake inspection icon from iconify

--- src/components/ServiceCard/ServiceCard.tsx
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -5,10 +5,12 @@ import "./ServiceCard.css";
 import DetailIcon from "../../assets/detail-icon.svg";
 import OilChangeIcon from "../../assets/oil-change-icon.svg";
 import TiresIcon from "../../assets/tires-icon.svg";
+import BrakeInspectionIcon from "../../assets/brake-inspection-icon.svg";
 
 
 const serviceIcons: Record<number, string> = {
   1: OilChangeIcon,
+  2: BrakeInspectionIcon,
   3: TiresIcon,
   4: DetailIcon,
 };
