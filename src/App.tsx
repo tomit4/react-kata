041a5b708e7972d7cb5cc99b07599a075c3f3934
tomit4 <mosssap@gmail.com>
2025-02-03 16:20:46 -0800

    :goal_net: Silenced react 18 and react-router warnings

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import Landing from "components/Landing";
 const App = () => {
   return (
     <main>
-      <BrowserRouter>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
         <Routes>
           <Route path="/" element={<Landing />} />
         </Routes>
